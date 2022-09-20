3d363b616ea3e8de336696d1e5d393be262e853a
H4V1 <javierxl20@hotmail.com>
2022-09-20 15:12:44 -0500

    update newGrid function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ for(i = 0; i < 16*16; i++) {
     container.appendChild(document.createElement('div'));
 }
 
-const div = document.querySelectorAll('div');
+let div = document.querySelectorAll('div');
 
 //add eventListener to every 'div' element
 div.forEach(function(element) {
@@ -19,11 +19,25 @@ const newGridBtn = document.querySelector('button');
 
 //remove all div elements and create a new grid using the number the user enter in the prompt
 function newGrid(size) {
-    div.forEach(element => element.remove());
     
+    //this removes the previous grid
+    div.forEach(element => element.remove());
+
+    //this creater the new grid based on the number entered in the prompt
     for(i = 0; i < size * size; i++) {
         container.appendChild(document.createElement('div'));
     }
+
+    //this defines the new grid to the variable div
+    div = document.querySelectorAll('div');
+
+    //this add eventListener to each div element
+    div.forEach(function(element) {
+        element.addEventListener('mouseover', function() {
+            element.style.backgroundColor = 'blue';
+        })
+    })
+    
 };
 
 //add eventListener to the New Grid button and pop up a prompt asking for a number less than 100 for the new grid
@@ -37,8 +51,3 @@ newGridBtn.addEventListener('click', function() {
     }
 });
 
-container.forEach(function(element) {
-    element.addEventListener('mouseover', function() {
-        element.style.backgroundColor = 'blue';
-    })
-})
