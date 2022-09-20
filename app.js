c8dbb7d44046923cc36ab7a3e84f98cf8fa77d99
H4V1 <javierxl20@hotmail.com>
2022-09-20 15:08:58 -0500

    add function to create new grid

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#container');
+let container = document.querySelector('#container');
 
 
 //Creates the first grid
@@ -15,15 +15,30 @@ div.forEach(function(element) {
     });
 });
 
-const newGrid = document.querySelector('button');
-let gridSize = 0;
+const newGridBtn = document.querySelector('button');
 
-newGrid.addEventListener('click', function() {
+//remove all div elements and create a new grid using the number the user enter in the prompt
+function newGrid(size) {
+    div.forEach(element => element.remove());
+    
+    for(i = 0; i < size * size; i++) {
+        container.appendChild(document.createElement('div'));
+    }
+};
+
+//add eventListener to the New Grid button and pop up a prompt asking for a number less than 100 for the new grid
+newGridBtn.addEventListener('click', function() {
     let num = prompt('Insert a number less than 100');
 
     if(num > 100) {
         alert('NUMBER IS GREATER THAN 100 TRY AGAIN!');
     } else {
-        gridSize = num;
+        newGrid(num);
     }
 });
+
+container.forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+        element.style.backgroundColor = 'blue';
+    })
+})
